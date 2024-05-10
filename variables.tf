bcddb6e12c8e8c4db45843004f1ac62b672536cb
Balu1000 <v.bh.ukr.net@gmail.com>
2024-05-10 19:23:21 +0000

    change

--- variables.tf
+++ variables.tf
@@ -5,25 +5,30 @@ variable "GOOGLE_PROJECT" {
 
 variable "GOOGLE_REGION" {
   type        = string
+  default     = "us-central1-c"
   description = "GCP region to use"
 }
 
 variable "GKE_NUM_NODES" {
   type        = number
+   default     = 2
   description = "GKE nodes number"
 }
 
 variable "NODE_MACHINE_TYPE" {
   type        = string
+  default     = "n1-standard-1"
   description = "Machine type for GKE nodes"
 }
 
 variable "GKE_CLUSTER_NAME" {
   type        = string
+  default     = "balu"
   description = "GKE cluster name"
 }
 
 variable "GKE_POOL_NAME" {
   type        = string
+  default     = "balu"
   description = "GKE pool name"
 }
